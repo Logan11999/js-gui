aa6916ada7b1979b0b6f960474369039837d2eda
Logan11999 <32521796+Logan11999@users.noreply.github.com>
2019-09-30 12:15:11 -0500

    added variables and started js

--- main.js
+++ main.js
@@ -1,51 +1,27 @@
-const add = (x, y) => { return x + y }
+function clock() {
+  var myTimer = setInterval(myClock, 1000);
+  var c = 0;
 
-const validate = async (event) => {
-  console.log(`triggered validate on ${event.target.id}`)
-  const isValid = event.target.checkValidity()
-  if (isValid) {
-    event.target.nextElementSibling.innerHTML = ''
-  } else {
-    event.target.nextElementSibling.innerHTML = 'Invalid input'
-    event.target.focus()
+  function myClock() {
+    document.getElementById("second").innerHTML = ++c;
+    if (c == 0) {
+      document.getElementById("second").innerHTML = 0;
+      document.getElementById("seconds").innerHTML = 1;
+    }
+    if (c==9) {
+      
+    }
   }
 }
 
-const updateWithAdd = async (event) => {
-  document.querySelector('#result').innerHTML = ''
-  if (document.querySelector('#firstNumber').checkValidity() && document.querySelector('#secondNumber').checkValidity()) {
-    const regex = /[^a-zA-Z_]/g
-    const s = document.querySelector('#guest').value.replace(regex, '')
-    const i = parseInt(document.querySelector('#firstNumber').value)
-    const j = parseInt(document.querySelector('#secondNumber').value)
-    const ans = `${s}, your sum is ${add(i, j)}.`
-    document.querySelector('#result').innerHTML = ans
-  }
-}
-
-const updateWithJoke = async (event) => {
-  document.querySelector('#result').innerHTML = ''
-  const url = 'https://api.icndb.com/jokes/random?limitTo=[nerdy]'
-  const response = await fetch(url)
-  const obj = await response.json()
-  const joke = obj.value.joke || 'No joke for you.'
-  document.querySelector('#result').innerHTML = joke
-}
-
-// delegate to dynamic elements (e.g. when testing)
-// focusout is like blur, but it bubbles up
-
-document.addEventListener('focusout', event => {
-  if ((event.target && event.target.id === 'firstNumber') ||
-    (event.target && event.target.id === 'secondNumber')) {
-    validate(event)
-  }
+document.addEventListener('click', event => {
+  if (event.target && event.target.id === 'start') {  }
 })
 
 document.addEventListener('click', event => {
-  if (event.target && event.target.id === 'addButton') { updateWithAdd(event) }
+  if (event.target && event.target.id === 'stop') {  }
 })
 
 document.addEventListener('click', event => {
-  if (event.target && event.target.id === 'getJokeButton') { updateWithJoke(event) }
+  if (event.target && event.target.id === 'reset') {  }
 })
